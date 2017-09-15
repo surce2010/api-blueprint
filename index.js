aebeee3874403ccd28f04375fad80dd00f5b9285
dingshaohua <345561171@qq.com>
2017-09-15 14:03:38 +0800

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('/'));
+app.use(express.static('./'));
 
 app.get('/', function(req, res) {
   res.send('API-BLUEPRINT');
